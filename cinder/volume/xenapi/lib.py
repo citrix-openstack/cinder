3746f2a760adace61dd029b43899813869e5defe
Mate Lakat <mate.lakat@citrix.com>
2012-11-06 10:43:05 +0000

    temp commit
    Change-Id: Id7ca15e307950a9f4384edd32236bb7cf066b778

--- cinder/volume/xenapi/lib.py
+++ cinder/volume/xenapi/lib.py
@@ -64,6 +64,8 @@ class XenAPISession(object):
     def create_sr(self, host_ref, device_config, name_label, name_description,
                   sr_type, physical_size=None, content_type=None,
                   shared=False, sm_config=None):
+        print name_label
+        print name_description
         return self.call_xenapi(
             'SR.create',
             host_ref,
